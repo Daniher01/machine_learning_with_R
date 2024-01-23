b13ba5d88bffcf20ebb8e223ab6dab4681e2ffdf
Daniel Hernandez <daniher02@gmail.com>
2024-01-23 12:30:43 -0300

    regresion lineal

--- Regresion Lineal/README.md
+++ Regresion Lineal/README.md
@@ -1,5 +1,29 @@
-# Analisis de Regresión
+# Analisis de Regresión Lineal
 
-## Técnica estadística para predecir el valor de una variable con los valores de otra
+Técnica estadística para predecir el valor de una variable con los valores de otra
 
 ![](resumen_RL.PNG)
+
+## Paso 1: Correlacion
+
+![](corr.PNG)
+
+## Paso 2: p-VALUE
+
+![](pvalue.PNG)
+
+## Paso 3: R cuadrado
+
+![](r2.PNG)
+
+## Paso 4: Tabla de coeficiente
+
+![](coeficientes_tabla.PNG)
+
+## Paso5: Construir la ecuacion
+
+![](ecuacion_RL.PNG)
+
+### output
+
+![](output.PNG)
---
+++ Regresion Lineal/regresion_lineal.R
@@ -0,0 +1,38 @@
+# EXPLORACION DE DATOS
+names(trabajadores)
+plot(trabajadores$salario_actual~trabajadores$salario_inicial)
+
+# Paso 1
+## Saber si existe una correlación lineal
+cor.test(trabajadores$salario_actual, trabajadores$salario_inicial)
+
+
+modelo1 <- lm(salario_actual~salario_inicial, data = trabajadores) # generar el modelo
+summary(modelo1) # paso 2,3,4
+
+# Paso 2
+## output
+# F-statistic:  1622 on 1 and 472 DF,  p-value: < 2.2e-16
+
+# Paso3 
+## output
+# Adjusted R-squared:  0.7741
+
+# Paso 4
+## output
+# Coefficients:
+#   Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)     1.928e+03  8.887e+02    2.17   0.0305 *  
+#   salario_inicial 1.909e+00  4.741e-02   40.28   <2e-16 ***
+
+# paso 5
+modelo1$coefficients
+## output
+# (Intercept) salario_inicial 
+# 1928.20576         1.90945 
+
+# construir la ecuacion
+1928.20 + (1.90 * 2100) # ejemplo de predicion para un salario inicial de 2100
+## output salario actual: 2918.2
+
+plot(trabajadores$salario_actual~trabajadores$salario_inicial) + abline(modelo1, col="red")
